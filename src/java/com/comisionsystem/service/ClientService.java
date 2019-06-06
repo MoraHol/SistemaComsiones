708d1a64d5384af097855a77b957a024783e0e1f
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-06-05 23:56:57 -0500

    trabajando en la rama

--- src/java/com/comisionsystem/service/ClientService.java
+++ src/java/com/comisionsystem/service/ClientService.java
@@ -22,21 +22,5 @@ public class ClientService {
         clientDAO = new ClientDaoImpl();
     }
     
-    public String convertSHA256(String password) {
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            return null;
-        }
-
-        byte[] hash = md.digest(password.getBytes());
-        StringBuilder sb = new StringBuilder();
-
-        for (byte b : hash) {
-            sb.append(String.format("%02x", b));
-        }
-
-        return sb.toString();
-    }
+    
 }
